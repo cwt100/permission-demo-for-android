d50bea70ca38057491dc75604cc38c8d79272a1a
WanTing Cheng <tingcheng@jsun.com>
2020-04-28 12:38:14 +0800

    Always deny for don't ask again to write sp for config to update message to suer setup.

--- app/src/main/java/com/example/permissiondemo/MainActivity.java
+++ app/src/main/java/com/example/permissiondemo/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.permissiondemo;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int PERMISSION_CAMERA_CODE = 1;
+
+    private static final String SP_CONFIG_KEY = "SP_CONFIG";
+    private static final String SP_ALWAYS_DENY_ENABLE_KEY = "SP_ALWAYS_DENY_ENABLE";
+
+    private boolean mIsAlwaysDenyEnable = false;
 
     private Button mCameraButton;
 
@@ -41,29 +48,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickCameraPermission(View view) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("需要相機權限");
-                builder.setMessage("");
-                builder.setPositiveButton("前往設定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA_CODE);
-                    }
-                });
-                builder.setNegativeButton("返回", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        updateCameraPermissionStatus();
-                        dialog.dismiss();
-                    }
-                });
-                builder.create();
-                builder.show();
-
-            }else {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA_CODE);
+                return;
             }
         }
     }
@@ -79,18 +67,44 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     updateCameraPermissionStatus();
                 }else {
-                    updateCameraPermissionStatus();
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("需要相機權限");
+                        builder.setMessage("");
+                        builder.setPositiveButton("前往設定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA_CODE);
+                            }
+                        });
+                        builder.setNegativeButton("返回", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                writeAlwaysDenyEnable(true);
+                                updateCameraPermissionStatus();
+                                dialog.dismiss();
+                            }
+                        });
+                        builder.create();
+                        builder.show();
+
+                    }else {
+                        writeAlwaysDenyEnable(true);
+                        updateCameraPermissionStatus();
+                    }
                 }
                 break;
         }
     }
 
     public void updateCameraPermissionStatus() {
+        readConfig();
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             mCameraButton.setEnabled(false);
             mCameraButton.setText("Camera permission is OK");
+            writeAlwaysDenyEnable(false);
         }else {
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA)) {
+            if (mIsAlwaysDenyEnable) {
                 mCameraButton.setEnabled(false);
                 mCameraButton.setText("請至設置介面打開權限");
             }else {
@@ -98,5 +112,18 @@ public class MainActivity extends AppCompatActivity {
                 mCameraButton.setText("Camera");
             }
         }
+    }
+
+    private SharedPreferences readConfig() {
+        SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences(SP_CONFIG_KEY, MODE_PRIVATE);
+        mIsAlwaysDenyEnable = sharedPreferences.getBoolean(SP_ALWAYS_DENY_ENABLE_KEY, false);
+        return sharedPreferences;
+    }
+
+    private void writeAlwaysDenyEnable(boolean isEnableAlwaysDeny) {
+        SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences(SP_CONFIG_KEY, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(SP_ALWAYS_DENY_ENABLE_KEY, isEnableAlwaysDeny);
+        editor.commit();
     }
 }
